b0be4d06ae625e4d741f4ff44043e91e607437b5
evgenyrinkevich <rinkevich2000@gmail.com>
2021-06-21 13:24:05 +0300

    Дз 2 урок ( добавил описание дз)

--- task_2.sql
+++ task_2.sql
@@ -1,3 +1,7 @@
+/*
+Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name.
+*/
+
 CREATE DATABASE example;
 USE example;
 CREATE TABLE users (
